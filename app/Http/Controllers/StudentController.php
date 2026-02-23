d33423a242d87e54f2891414c22e9d5dd76d24ef
unknown <muchammad.faisal@unisba.ac.id>
2026-02-23 13:10:06 +0700

    add method read and process in studentController

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -23,6 +23,8 @@ class StudentController extends Controller
 {
     /**
      * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
      */
     public function index(Request $request)
     {
@@ -34,6 +36,7 @@ class StudentController extends Controller
         $classofs = [0 => 'Semua'] + $classofs->toArray();
         $status = [0 => 'Semua', 1 => 'Aktif', 'Tidak Aktif'];
         $pansus = [0 => 'Semua', 'No', 'Yes'];
+
 
         $search_text = $request->search_text;
         $search_gender = $request->search_gender ? $request->search_gender : 0;
@@ -49,7 +52,7 @@ class StudentController extends Controller
                 select sum(sks) 
                 from student_activities 
                 join sub_activities on sub_activities.id = student_activities.sub_activity_id 
-                where student_activities.student_id = students.id and student_activities.status = 3) as sumsks')
+                where student_activities.student_id = students.id and  student_activities.status = 3) as sumsks')
             ->where(function ($q) use ($search_text) {
                 $q->whereRaw('name like ?', ['%' . $search_text . '%'])
                     ->orWhereRaw('npm like ?', ['%' . $search_text . '%']);
@@ -91,6 +94,8 @@ class StudentController extends Controller
 
     /**
      * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
      */
     public function create()
     {
@@ -107,6 +112,9 @@ class StudentController extends Controller
 
     /**
      * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
      */
     public function store(Request $request)
     {
@@ -180,6 +188,9 @@ class StudentController extends Controller
 
     /**
      * Display the specified resource.
+     *
+     * @param  \App\Models\Student  $student
+     * @return \Illuminate\Http\Response
      */
     public function show(Student $student)
     {
@@ -207,6 +218,9 @@ class StudentController extends Controller
 
     /**
      * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Student  $student
+     * @return \Illuminate\Http\Response
      */
     public function edit(Student $student)
     {
@@ -234,6 +248,10 @@ class StudentController extends Controller
 
     /**
      * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Student  $student
+     * @return \Illuminate\Http\Response
      */
     public function update(Request $request, Student $student)
     {
@@ -307,6 +325,9 @@ class StudentController extends Controller
 
     /**
      * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Student  $student
+     * @return \Illuminate\Http\Response
      */
     public function destroy(Student $student)
     {
@@ -330,12 +351,297 @@ class StudentController extends Controller
 
     public function read(Request $request)
     {
-        // Method read existing...
+        $result = ['message' => 'error', 'data' => 'Ups, terjadi kesalahan, refresh lalu coba lagi.'];
+
+        $f_name = $request->cfu_file;
+        $index = $request->index;
+        $req_type = $request->req_type;
+
+        if (!$req_type) {
+            $file_name = 'import-students-' . date('YmdHis') . '.xlsx';
+            $folder_path = storage_path('app/public/files/temp');
+            $fileSystem = new Filesystem();
+            if (!$fileSystem->exists($folder_path)) {
+                $fileSystem->makeDirectory($folder_path, 0777, true, true);
+            }
+
+            $file_path = $folder_path . '/' . $file_name;
+            $fileSystem->move($f_name->getPathname(), $file_path);
+
+            try {
+                $contents = '';
+                $excel_rows = new DataImport();
+                FacadesExcel::import($excel_rows, 'public/files/temp/' . $file_name);
+                $rows = $excel_rows->getExcelRows();
+                $filtered_rows = [];
+                $row_count = 0;
+
+                if (count($rows) > 0) {
+                    foreach ($rows as $row) {
+                        $emptyflag = false;
+                        for ($cnt = 0; $cnt < 10; $cnt++) {
+                            if ($row[$cnt] !== null && $row[$cnt] !== '') {
+                                $emptyflag = true;
+                            }
+                        }
+
+                        if ($emptyflag) {
+                            $filtered_rows[] = $row->toArray();
+                            $row_count++;
+                        }
+                    }
+                }
+
+                $total_data = $row_count - 1;
+                $request->session()->forget('importStudents');
+                $request->session()->forget('arr_type');
+                $request->session()->put('importStudents', $filtered_rows);
+                $fileSystem->delete($file_path);
+                return response()->json(array('status' => 'success', 'counts' => $row_count));
+            } catch (\Exception $e) {
+                $fileSystem->delete($file_path);
+                return response()->json(array('status' => 'failed', 'err_msg' => 'Error reading file!'));
+            }
+        } else {
+            $rows = $request->session()->get('importStudents');
+            $header = $rows[0];
+
+            if ($header[0] == 'NPM' && $header[1] == 'Nama' && $header[2] == 'Angkatan' && $header[3] == 'Jenis Kelamin' && $header[4] == 'Agama' && $header[5] == 'Telepon' && $header[6] == 'Email' && $header[7] == 'Tanggal Lahir' && $header[8] == 'Alamat' && $header[9] == 'Periode Pengisian') {
+                $table_contents = $index == 0 ? '<thead>' : ($index == 1 ? '<tbody>' : '');
+
+                if ($rows) {
+                    $contents = $rows[$index];
+
+                    $flagEmpty = true;
+                    $flagNumber = true;
+                    $flaginvalid = true;
+
+                    for ($a = 0; $a < 10; ++$a) {
+                        if ($contents[$a] === '' || $contents[$a] === null) {
+                            $flagEmpty = false;
+                        }
+
+                        if ($index > 0) {
+                            if (in_array($a, [3, 4])) {
+                                if (!is_numeric($contents[$a])) {
+                                    $flagNumber = false;
+                                } else {
+                                    if (!in_array($contents[3], [1, 2])) {
+                                        $flaginvalid = false;
+                                    }
+
+                                    if (!in_array($contents[4], [1, 2, 3, 4, 5, 6])) {
+                                        $flaginvalid = false;
+                                    }
+                                }
+                            }
+                        }
+                    }
+
+                    $dupliflag = true;
+                    $coll = array_filter(array_column($rows, 0));
+                    $collemail = array_filter(array_column($rows, 6));
+                    $dupli = $dupliemail = 0;
+
+                    if ($contents[0] !== null && $contents[0] !== '') {
+                        $dupli = array_count_values($coll)[$contents[0]];
+                    }
+
+                    if ($contents[6] !== null && $contents[6] !== '') {
+                        $dupliemail = array_count_values($collemail)[$contents[6]];
+                    }
+
+                    if ($dupli > 1 || $dupliemail > 1) {
+                        $dupliflag = false;
+                    }
+
+                    if ($dupliflag === true) {
+                        $student = Student::where('npm', $contents[0])->first();
+                        if ($student === null) {
+                            if ($flagEmpty == true) {
+                                if ($flagNumber == true) {
+                                    if ($flaginvalid == true) {
+                                        $table_contents .= '<tr id="tr-import-' . $index . '">';
+                                    } else {
+                                        $table_contents .= '<tr id="tr-import-' . $index . '" class="location_number_wrong" style="background:#2f7ba1" title="There is data that is wrong value !!">';
+                                    }
+                                } else {
+                                    $table_contents .= '<tr id="tr-import-' . $index . '" class="location_number" style="background:#ff9f30" title="There is data that is not a number !!">';
+                                }
+                            } else {
+                                $table_contents .= '<tr id="tr-import-' . $index . '" class="location_data_empty" style="background:#6db774" title="There is data that is still empty !!">';
+                            }
+                        } else {
+                            $table_contents .= '<tr id="tr-import-' . $index . '" class="student_exist" style="background:#ffca68" title="Mahasiswa sudah ada !!">';
+                        }
+                    } else {
+                        $table_contents .= '<tr id="tr-import-' . $index . '" class="student_duplicate" style="background:#a35252" title="Duplicated Student or duplicate email !!">';
+                    }
+
+                    $genders = ["1" => "Laki - Laki", "Perempuan"];
+                    $religions = ["1" => "Islam", "Hindu", "Budha", "Kristen", "Protestan", "Kepercayaan"];
+
+                    for ($cnt = 0; $cnt < 10; $cnt++) {
+                        $tid = '';
+                        if ($cnt == 0) {
+                            $tid = ' id="td-import-' . $index . '" ';
+                        }
+
+                        $toappear = $contents[$cnt];
+                        if (in_array($cnt, [7, 9]) && $index != 0) {
+                            $dt = Carbon::instance(Date::excelToDateTimeObject($contents[$cnt]));
+                            $toappear = date("d F Y", strtotime($dt));
+                        }
+
+                        if ($index != 0 && $cnt == 3) {
+                            if ($contents[$cnt] != "" && is_numeric($contents[$cnt]) && $contents[$cnt] < 3) {
+                                $toappear = $genders[$contents[$cnt]];
+                            }
+                        } elseif ($index != 0 && $cnt == 4) {
+                            if ($contents[$cnt] != "" && is_numeric($contents[$cnt]) && $contents[$cnt] < 7) {
+                                $toappear = $religions[$contents[$cnt]];
+                            }
+                        }
+
+                        $table_contents .= '<td ' . $tid . '>&nbsp;&nbsp;&nbsp;' . $toappear . '</td>';
+                    }
+
+                    $table_contents .= '</tr>';
+                    $table_contents .= $index == 0 ? '</thead>' : ($index + 1 == count($rows) ? '</tbody>' : '');
+                    return response()->json(array('status' => 'success', 'content' => $table_contents, 'row' => count($rows)));
+                } else {
+                    return response()->json(array('status' => 'failed', 'err_msg' => ' Session has expired!'));
+                }
+            } else {
+                return response()->json(array('status' => 'failed', 'err_msg' => 'Imported file formats are not allowed!'));
+            }
+        }
+
+        if ($request->ajax()) {
+            return response()->json($result);
+        }
+        return Redirect::to('/');
     }
 
     public function process(Request $request)
     {
-        // Method process existing...
+        $result = ['message' => 'error', 'data' => 'Ups, terjadi kesalahan, refresh lalu coba lagi.'];
+
+        $index = $request->index;
+        $rows = $request->session()->get('importStudents');
+
+        if ($rows) {
+            $contents = $rows[$index];
+
+            $flagEmpty = true;
+            $flagNumber = true;
+            $flaginvalid = true;
+
+            for ($a = 0; $a < 10; ++$a) {
+                if ($contents[$a] === '' || $contents[$a] === null) {
+                    $flagEmpty = false;
+                }
+
+                if ($index > 0) {
+                    if (in_array($a, [3, 4])) {
+                        if (!is_numeric($contents[$a])) {
+                            $flagNumber = false;
+                        } else {
+                            if (!in_array($contents[3], [1, 2])) {
+                                $flaginvalid = false;
+                            }
+
+                            if (!in_array($contents[4], [1, 2, 3, 4, 5, 6])) {
+                                $flaginvalid = false;
+                            }
+                        }
+                    }
+                }
+            }
+
+            if ($index > 0) {
+
+                $dupliflag = true;
+                $coll = array_filter(array_column($rows, 0));
+                $collemail = array_filter(array_column($rows, 6));
+                $dupli = $dupliemail = 0;
+
+                if ($contents[0] !== null && $contents[0] !== '') {
+                    $dupli = array_count_values($coll)[$contents[0]];
+                }
+
+                if ($contents[6] !== null && $contents[6] !== '') {
+                    $dupliemail = array_count_values($collemail)[$contents[6]];
+                }
+
+                if ($dupli > 1 || $dupliemail > 1) {
+                    $dupliflag = false;
+                }
+
+                if ($dupliflag === true) {
+                    $student = Student::where('npm', $contents[0])->first();
+                    if ($student === null) {
+                        if ($flagEmpty == true) {
+                            if ($flagNumber == true) {
+                                if ($flaginvalid == true) {
+                                    DB::transaction(function () use ($contents) {
+                                        $message = new Student();
+                                        $message->npm = $contents[0];
+                                        $message->name = $contents[1];
+                                        $message->class_of = $contents[2];
+                                        $message->order = $message->lastOrder($contents[2]) + 1;
+                                        $message->gender = $contents[3];
+                                        $message->religion = $contents[4];
+                                        $message->phone = $contents[5];
+                                        $message->email = $contents[6];
+                                        if ($contents[7] !== null && $contents[7] != '') {
+                                            $dt = Carbon::instance(Date::excelToDateTimeObject($contents[7]));
+                                            $message->date_of_birth = $dt;
+                                        }
+                                        if ($contents[8] !== null && $contents[8] != '') {
+                                            $message->address = $contents[8];
+                                        }
+                                        if ($contents[9] !== null && $contents[9] != '') {
+                                            $dt = Carbon::instance(Date::excelToDateTimeObject($contents[9]));
+                                            $message->period = $dt;
+                                        }
+                                        $message->creator = auth()->user()->username;
+                                        $message->editor = auth()->user()->username;
+                                        $message->save();
+
+                                        $user = new User();
+                                        $user->name = $contents[1];
+                                        $user->username = $contents[0];
+                                        $user->email = $contents[6];
+                                        $user->password = Hash::make("passwordnyabelumdisetting");
+                                        $user->creator = auth()->user()->username;
+                                        $user->editor = auth()->user()->username;
+                                        $message->user()->save($user);
+                                    });
+
+                                    $result = ['message' => 'success', 'data' => 'Student ' . $contents[0] . ' has been added!!', 'index' => $index];
+                                } else {
+                                    $result = ['message' => 'error', 'data' => 'There is data that is wrong value !!', 'index' => $index];
+                                }
+                            } else {
+                                $result = ['message' => 'error', 'data' => 'There is data that is not a number !!', 'index' => $index];
+                            }
+                        } else {
+                            $result = ['message' => 'error', 'data' => 'There is data that is still empty !!', 'index' => $index];
+                        }
+                    } else {
+                        $result = ['message' => 'error', 'data' => 'Mahasiswa sudah ada!!', 'index' => $index];
+                    }
+                } else {
+                    $result = ['message' => 'error', 'data' => 'Duplicate Student or email!!', 'index' => $index];
+                }
+            }
+        }
+
+        if ($request->ajax()) {
+            return response()->json($result);
+        }
+        return Redirect::to('/');
     }
 
     public function exportStudents(Request $request)
@@ -345,6 +651,7 @@ class StudentController extends Controller
 
     public function approveCertificate(Request $request)
     {
+
         $student = Student::where("id", $request->id)->first();
         if ($student !== null) {
             $student->certificate_approve = 1;
@@ -361,8 +668,8 @@ class StudentController extends Controller
     {
         $student = Student::where("id", $request->id)->first();
         if ($student !== null) {
-            $student->certificate_approve = 0;
-            $student->certificate_approve_date = null;
+            $student->certificate_approve = 0; // atau status reject
+            $student->certificate_approve_date = null; // kosongkan tanggal
             $student->save();
 
             return response()->json(['status' => 'ok']);
@@ -541,8 +848,12 @@ class StudentController extends Controller
                     $kegiatanKelompok[] = [
                         'nama' => $activity['nama'],
                         'min_wajib' => $activity['min_wajib'],
-                        'kegiatan' => $activity['kegiatan']
-                    ];
+                        'kegiatan' => $activity['kegiatan'],
+                        'wajib_terpenuhi'    => $activity['kegiatan_wajib_terpenuhi'],
+                        'wajib_total'        => $activity['kegiatan_wajib_count'],
+                        'total_kegiatan'     => count($activity['kegiatan']),
+                        'terpenuhi_kegiatan' => collect($activity['kegiatan'])->where('status', 'TERPENUHI')->count()
+                        ];
                 }
 
                 return [
