d9beaefcf90f249951ee194a7b99674d5397b488
unknown <muchammad.faisal@unisba.ac.id>
2026-02-26 16:53:05 +0700

    fixed toggle modal student index

--- resources/views/pages/students/index.blade.php
+++ resources/views/pages/students/index.blade.php
@@ -24,7 +24,7 @@
 
         <div class="card-header-right">
             <div class="btn-group card-option">
-                <button type="button" class="btn dropdown-toggle btn-icon" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                <button type="button" class="btn dropdown-toggle btn-icon" data-toggle="modal" data-target="#exampleModal">
                     <i class="feather icon-more-horizontal"></i>
                 </button>
                 <ul class="list-unstyled card-option dropdown-menu dropdown-menu-right">
